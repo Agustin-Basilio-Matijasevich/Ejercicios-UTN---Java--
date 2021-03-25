998da0978d3c135bee44fe1c0d14d4284898e104
Agustin Basilio Matijasevich <agustinbasiliomatijasevich@gmail.com>
2021-03-25 09:12:18 -0300

    Pruebitas con in por consola

--- Experimentos-a/src/main/java/com/mycompany/experimentos/a/Experimento.java
+++ Experimentos-a/src/main/java/com/mycompany/experimentos/a/Experimento.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.experimentos.a;
 
+import java.util.Scanner;
+
 /**
  *
  * @author agust
@@ -17,8 +19,18 @@ public class Experimento {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        Scanner entrada=new Scanner(System.in);
+        
+        int a;
+        
         System.out.println("Hola Mundo\n");
         System.out.println("Que tal?");
+        
+        System.out.println("Ingrese a: ");
+        
+        a = entrada.nextInt();
+        
+        System.out.println("a=" + a);
     }
     
 }
