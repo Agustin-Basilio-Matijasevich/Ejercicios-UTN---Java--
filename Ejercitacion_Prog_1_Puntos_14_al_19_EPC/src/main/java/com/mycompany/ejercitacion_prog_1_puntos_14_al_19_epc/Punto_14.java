b0ab4d64928ef5e8d4634c6992261672eeb9596e
Basilio Matijasevich, Agustín <81303198+Agustin-Basilio-Matijasevich@users.noreply.github.com>
2021-03-27 01:11:35 -0300

    Se modifico el archivo y se puso un hola

---
+++ Ejercitacion_Prog_1_Puntos_14_al_19_EPC/src/main/java/com/mycompany/ejercitacion_prog_1_puntos_14_al_19_epc/Punto_14.java
@@ -0,0 +1,14 @@
+package com.mycompany.ejercitacion_prog_1_puntos_14_al_19_epc;
+
+/**
+ *
+ * @author agust
+ */
+public class Punto_14
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hola");
+    }
+    
+}
